dc66d9de18ed2e9758a41c6b74c4c641cdd4d561
Maria Eduarda Massa <djsheepwolfbr@gmail.com>
2022-09-06 18:15:40 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao2;
+
+/**
+ *
+ * @author mariaeduardamassa
+ */
+public class Aluno {
+    private String nome;
+    private double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the nota
+     */
+    public double getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the nota to set
+     */
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+}
